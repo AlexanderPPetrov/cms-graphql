b14b89cd9762bdcf5aeea9f45599e40025d1c8f9
aleksandar.p <aleksandar.p@delasport.com>
2021-01-08 11:32:12 +0200

    basic error handling

---
+++ layouts/error.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-card>
+        <v-alert v-if="error.statusCode === 404"
+                 outlined
+                 prominent
+                 type="warning"
+        >
+            Page not found
+            <div>
+                <NuxtLink to="/">Dashboard</NuxtLink>
+            </div>
+        </v-alert>
+        <v-alert v-else
+                 type="error"
+                 outlined
+                 text
+        >
+            An error occurred
+            <div>
+                <NuxtLink to="/">Dashboard</NuxtLink>
+            </div>
+        </v-alert>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error' // you can set a custom layout for the error page
+}
+</script>
